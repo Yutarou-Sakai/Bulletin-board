50ce18ac1deae5d2421d965bda886a03c6c7af40
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-19 19:17:49 +0900

    boardsのshowアクションを作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # root 'boards#index'
 
   get 'boards', to: 'boards#index'
+  get 'boards/:id', to: 'boards#show'
   get 'boards/new', to: 'boards#new'
   post 'boards', to: 'boards#create'
 end
