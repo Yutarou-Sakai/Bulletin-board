79c94a36647ef9bbb9a22e39af42eeb842944309
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-20 00:01:52 +0900

    コメントの削除機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.delete
+
+    redirect_to comment.board, notice: 'コメントを削除しました'
   end
 
   private
