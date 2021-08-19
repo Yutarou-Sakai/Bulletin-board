9c16315d02065a24fda7464dc9b0b187d099c20e
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-19 21:02:24 +0900

    boardの各ページのurlをURL用ヘルパーに変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
   # root 'boards#index'
 
-  get 'boards', to: 'boards#index'
-  get 'boards/:id', to: 'boards#show'
-  get 'boards/new', to: 'boards#new'
-  post 'boards', to: 'boards#create'
+  resources :boards, only: [:index, :show, :new, :create]
 end
