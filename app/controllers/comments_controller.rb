0ff2a998c3dd63789e7b095ab9fc1db244e4aa89
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-19 23:39:15 +0900

    コメント保存機能を実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,21 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to comment.board, notice: "コメントを作成しました"
+    else
+      redirect_to :back, flash: {
+        comment: comment,
+        error_messages: comment.errors.full_messages
+      }
+    end
   end
 
   def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:board_id, :name, :comment)
   end
 end
