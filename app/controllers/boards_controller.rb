2953f2e892806adb00cf5acd0a56f37263354c10
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-19 21:28:38 +0900

    boardの削除機能を実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -12,7 +12,9 @@ class BoardsController < ApplicationController
   end
 
   def create
-    Board.create(board_params)
+    board = Board.create(board_params)
+
+    redirect_to board
   end
 
   def edit
@@ -24,6 +26,13 @@ class BoardsController < ApplicationController
     board.update(board_params)
 
     redirect_to board
+  end
+
+  def destroy
+    board = Board.find(params[:id])
+    board.delete
+
+    redirect_to boards_path
   end
 
   private
