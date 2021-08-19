3abd348b67a4be35cb3a2e92755a5afc5581f688
Yutarou-Sakai <s.yutarou2718@gmail.com>
2021-08-19 22:40:45 +0900

    boardモデルのバリデーションを設定

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,13 +9,19 @@ class BoardsController < ApplicationController
   end
 
   def new
-    @board = Board.new
+    @board = Board.new(flash[:board])
   end
 
   def create
-    board = Board.create(board_params)
-
-    redirect_to board, notice: "#{board.title} の掲示板を作成しました"
+    board = Board.new(board_params)
+    if board.save
+      redirect_to board, notice: "#{board.title} の掲示板を作成しました"
+    else
+      redirect_to new_board_path, flash: {
+        board: board,
+        error_messages: board.errors.full_messages
+      }
+    end
   end
 
   def edit
